4af1b0c032de3eb5a70001c8abfa22b528ee9bd9
Hailey Miller <hmiller@laurelroad.com>
2019-10-31 17:02:00 -0400

    make the navbar pretty

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default function NavBar() {
   return (
-    <nav>
-      Nav goes up here.
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">ESAP Needle Map</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse className="justify-content-end">
+     <Nav>
+        <Nav.Link href="#map">Map</Nav.Link>
+        <Nav.Link href="#about" >About</Nav.Link>
+      </Nav>
+     </Navbar.Collapse>
+    </Navbar>
   );
 }
