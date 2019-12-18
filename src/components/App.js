17210b9bce8db4496083f76d7ba944d367de61c3
mermatriarchy <hmiller@laurelroad.com>
2019-12-18 18:47:39 -0500

    fix file placement, add docs & json

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import NavBar from "./NavBar";
+import NavBar from "./common/NavBar";
 import GoogleMap from "./GoogleMap";
 import PageNotFound from "./PageNotFound";
-import About from "./common/About";
+import About from "./About";
 import Footer from "./common/Footer";
 
 export default function App() {
